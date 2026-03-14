b631ebd1356ecb69b300b87a60d4e625f7051c1b
mphung97 <mphung97@github.com>
2026-03-14 13:29:56 +0700

    Add configuration options for multiple workshop mods in modoverrides.lua

--- Caves/modoverrides.lua
+++ Caves/modoverrides.lua
@@ -1,6 +1,80 @@
 return {
+    ["workshop-1111658995"] = {
+        configuration_options = {},
+        enabled = true
+    },
+    ["workshop-1185229307"] = {
+        configuration_options = {
+            CAMERA = true,
+            CAPTURE = false,
+            DAMAGE_NUMBERS = true,
+            DAMAGE_RESISTANCE = true,
+            FRAME_PHASES = true,
+            GLOBAL = true,
+            GLOBAL_NUMBERS = true,
+            HEADER_CLIENT = false,
+            HEADER_SERVER = false,
+            HORIZONTAL_OFFSET = 0,
+            TAG = "_HEALTH",
+            WETNESS_METER = true
+        },
+        enabled = true
+    },
+    ["workshop-1852257480"] = {
+        configuration_options = {
+            horizontal = 0,
+            info = true,
+            position = "bottom",
+            vertical = "auto"
+        },
+        enabled = true
+    },
+    ["workshop-2078243581"] = {
+        configuration_options = {
+            Blue = 0,
+            Display = "target",
+            Green = 0,
+            Projectile = true,
+            Red = 1,
+            Type = "hit"
+        },
+        enabled = true
+    },
+    ["workshop-347079953"] = {
+        configuration_options = {
+            DFV_Language = "EN",
+            DFV_MinimalMode = "default"
+        },
+        enabled = true
+    },
+    ["workshop-362175979"] = {
+        configuration_options = {
+            ["Draw over FoW"] = "disabled"
+        },
+        enabled = true
+    },
+    ["workshop-374550642"] = {
+        configuration_options = {
+            FORCE_STACKSIZES = false,
+            STACK_SIZE_LARGEITEM = 99,
+            STACK_SIZE_MEDITEM = 99,
+            STACK_SIZE_PELLET = 120,
+            STACK_SIZE_SMALLITEM = 99,
+            STACK_SIZE_TINYITEM = 99
+        },
+        enabled = true
+    },
     ["workshop-375850593"] = {
         configuration_options = {},
+        enabled = true
+    },
+    ["workshop-501385076"] = {
+        configuration_options = {
+            quick_cook_on_fire = true,
+            quick_harvest = true,
+            quick_pick_cactus = true,
+            quick_pick_plant_normal_ground = true
+        },
         enabled = true
     }
 }
