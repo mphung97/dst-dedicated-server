ff95641b1544112d913c2bfc702f53e18a264f23
mphung97 <mphung97@github.com>
2026-03-14 23:32:25 +0700

    Remove deprecated mod files and add new mod setups for additional equip slots

--- mods/dedicated_server_mods_setup.lua
+++ mods/dedicated_server_mods_setup.lua
@@ -1,12 +1,11 @@
---There are two functions that will install mods, ServerModSetup and ServerModCollectionSetup. Put the calls to the functions in this file and they will be executed on boot.
-
---ServerModSetup takes a string of a specific mod's Workshop id. It will download and install the mod to your mod directory on boot.
-	--The Workshop id can be found at the end of the url to the mod's Workshop page.
-	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=350811795
-	--ServerModSetup("350811795")
-
---ServerModCollectionSetup takes a string of a specific mod's Workshop id. It will download all the mods in the collection and install them to the mod directory on boot.
-	--The Workshop id can be found at the end of the url to the collection's Workshop page.
-	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=379114180
-	--ServerModCollectionSetup("379114180")
-ServerModCollectionSetup("3684494853")
+ServerModSetup("1852257480")
+ServerModSetup("569043634")
+ServerModSetup("2078243581")
+ServerModSetup("347079953")
+ServerModSetup("1185229307")
+ServerModSetup("375850593")
+ServerModSetup("378160973")
+ServerModSetup("374550642")
+ServerModSetup("501385076")
+ServerModSetup("1111658995")
+ServerModSetup("362175979")
